a48b422258bfae867255de33f7f5006c302faa23
vaishnavi9850 <121082141+vaishnavi9850@users.noreply.github.com>
2023-09-05 23:39:35 +0530

    Files Added

---
+++ Javascript.js
@@ -0,0 +1,36 @@
+
+
+// 1st Package
+    $(document).ready(function(){
+        $("#btn1").click(function()
+        {
+            $("#pack1").slideToggle();
+        });
+    });
+
+// 2 nd Package 
+$(document).ready(function(){
+    $("#btn2").click(function()
+    {
+        $("#pack2").slideToggle();
+    });
+});
+
+// 3rd Package 
+$(document).ready(function(){
+    $("#btn3").click(function()
+    {
+        $("#pack3").slideToggle();
+    });
+});
+
+// SignUp function 
+function signup()
+{
+    alert("Sign Up Successfully");
+}
+
+// book function
+
+
+
